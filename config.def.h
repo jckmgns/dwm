2e83d3fb40d90e43bf993e773933ccf4bc8abb6a
Jack Magnus <git@jackmagnus.cc>
2024-05-31 14:41:12 +0200

    Change config
    - Disable pixel border (since picom inactive dim is clearer).
    - Add basic tag assignment for appliations (mostly work related).
    - Lower the master area size back to 55%.

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -29,13 +29,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class                    instance    title       tags mask   isfloating   monitor */
+	{ "Firefox-esr",            NULL,       NULL,       0,          0,           -1 },
+	{ "thunderbird-default",    NULL,       NULL,       1 << 2,     0,           -1 },
+	{ "jetbrains-idea-ce",      NULL,       NULL,       1 << 7,     0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.6;  /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55;  /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
